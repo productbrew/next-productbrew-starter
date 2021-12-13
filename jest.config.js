d9b548c43780739594bd277ec83fb03d478ed4c9
czystyl <lukasz.czyszczonik@gmail.com>
2021-12-13 16:25:36 +0100

    Upgrade to Next 12

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,20 @@
-module.exports = {
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-  },
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "<rootDir>/styles/__mocks__/styleMock.js",
-    "\\.(css|less|scss|sass)$": "<rootDir>/styles/__mocks__/styleMock.js",
-    "^@pages/(.*)$": "<rootDir>/pages/$1",
+    // Handle module aliases (this will be automatically configured for you soon)
     "^@components/(.*)$": "<rootDir>/components/$1",
+    "^@pages/(.*)$": "<rootDir>/pages/$1",
+    "^@styles/(.*)$": "<rootDir>/styles/$1",
   },
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
